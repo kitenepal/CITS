da3d65a33384b8786ea5e1f2c90f55170328e1ff
skapali66 <skapali66@gmail.com>
2018-07-17 12:15:41 +0545

    added about us section and team slider

---
+++ js/header.js
@@ -0,0 +1,25 @@
+$(window).scroll(()=>{
+	var windowTop = $(window).scrollTop();
+	windowTop > 70 ? $('#scroll-nav').removeClass('trans-back') : $('#scroll-nav').addClass('trans-back');
+
+
+});
+
+
+$('nav ul>li>a[href*="#"]').on('click', function(e){
+    $('html,body').animate({
+        scrollTop: $($(this).attr('href')).offset().top - 65
+    },500);
+    e.preventDefault();
+});
+
+
+/*
+$(function(){
+  $('.group').on('mouseover',function(){
+    $('.main-image').css({'background':'url(img/web.jpg)','background-position':'top center','background-size':'cover'});
+  });
+  $('.group').on('mouseleave',function(){
+    $('.main-image').css({'background':'','background-position':'','background-size':''});
+  });
+});*/
