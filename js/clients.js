d5cfe2b3c900c4f67ff3cadec57ae9bf242b5799
NidanStha <nidanshrestha@gmail.com>
2018-07-19 15:20:43 +0545

    nav and course connected by jq

--- js/clients.js
+++ js/clients.js
@@ -8,3 +8,15 @@ $(document).ready(function() {
         $("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
     });
 });
+
+$(document).ready(function() {
+    $(".nav-serv>.course").click(function(e) {
+        e.preventDefault();
+        var panelId=$(this).attr('data-panel');
+        $('#'+panelId).siblings('a.active').removeClass("active");
+        $('#'+panelId).addClass("active");
+        var index = $(this).index();
+        $("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
+        $("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
+    });
+});
